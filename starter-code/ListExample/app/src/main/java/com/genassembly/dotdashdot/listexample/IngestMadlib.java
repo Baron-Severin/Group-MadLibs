8d223dbfc15bb1fedff99b71ea8df30af2b4b9c9
Baron-Severin <severinrudie@gmail.com>
2016-07-07 22:23:55 -0700

    Finished most of IngestMadlib.java

--- starter-code/ListExample/app/src/main/java/com/genassembly/dotdashdot/listexample/IngestMadlib.java
+++ starter-code/ListExample/app/src/main/java/com/genassembly/dotdashdot/listexample/IngestMadlib.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -27,6 +29,8 @@ public class IngestMadlib extends AppCompatActivity {
     TextToSpeech textToSpeech;
     String currentDisplayText;
     ArrayList<String> currentTextChunks;
+    String[] spinnerContents;
+    boolean lastAddedWasString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,16 +57,31 @@ public class IngestMadlib extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.constructorSpinnerSpace);
         buttonSpinner = (Button) findViewById(R.id.constructorButtonForSpinner);
         resetAll = (Button) findViewById(R.id.constructorButtonResetInput);
-
+//        spinnerInts = new int[]{R.string.ADJECTIVE, R.string.ANIMAL, R.string.GAME, R.string.NAME, R.string.NOUN, R.string.VERB};
+        spinnerContents = new String[] {"*ADJECTIVE*", "*ANIMAL*", "*GAME*", "*NAME*", "*NOUN*", "*VERB*"};
+        ArrayAdapter<String> stringAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerContents);
+        stringAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(stringAdapter);
+        lastAddedWasString = false;
     }
 
     public void addMLText(View view) {
 
-        if (validateMLText()) {
+        if (lastAddedWasString) {
+            String message = "Please enter a space before adding more text";
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            textToSpeech.setSpeechRate(.9f);
+            if (!textToSpeech.isSpeaking()) {
+                textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        } else if (validateMLText()) {
+            // add text
             String newChunk = editMadlib.getText().toString();
             currentDisplayText += " " + newChunk;
             currentTextChunks.add(newChunk);
             textView.setText(currentDisplayText);
+            editMadlib.setText("");
+            lastAddedWasString = true;
         } else {
             String message = "Please enter text into the field";
             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
@@ -70,6 +89,24 @@ public class IngestMadlib extends AppCompatActivity {
             if (!textToSpeech.isSpeaking()) {
                 textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
             }
+        }
+    }
+
+    public void addSpace (View view) {
+
+        if (!lastAddedWasString){
+            String message = "Please enter text before adding another space";
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            textToSpeech.setSpeechRate(.9f);
+            if (!textToSpeech.isSpeaking()) {
+                textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        } else {
+            String newChunk = spinner.getSelectedItem().toString();
+            currentDisplayText += " " + newChunk;
+            currentTextChunks.add(newChunk);
+            textView.setText(currentDisplayText);
+            lastAddedWasString = false;
         }
     }
 
@@ -81,4 +118,15 @@ public class IngestMadlib extends AppCompatActivity {
         }
     }
 
+    public void resetAllText(View view) {
+        currentTextChunks = new ArrayList<String>();
+        currentDisplayText = "";
+        editMadlib.setText("");
+        lastAddedWasString = false;
+        textView.setText(currentDisplayText);
+    }
+    public void finishML(View view) {
+        // send off the arraylist
+        resetAllText(view);
+    }
 }
